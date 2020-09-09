3436dc4023c32366e00b31042a8111e981e8b6f5
chiranjit@ascentspark.com <chiranjit@ascentspark.com>
2020-09-09 18:46:12 +0530

    Fastlane added

--- ml-cicd/Controllers/ImageViewController.swift
+++ ml-cicd/Controllers/ImageViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import NSFWDetector
+import QuartzCore
 
 class ImageViewController: UIViewController {
 
@@ -14,21 +16,35 @@ class ImageViewController: UIViewController {
     @IBOutlet private weak var blurView: UIVisualEffectView!
     @IBOutlet private weak var nsfwLabel: UILabel!
     
+    var image: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        blurView.layer.cornerRadius = 10
+        blurView.clipsToBounds = true
+        
+        guard let image = self.image else {
+            self.nsfwLabel.text = "No Image Selected"
+            return
+        }
+        
+        self.imageView.image = image
+        
+        NSFWDetector.shared.check(image: image) { [weak self] (result) in
+            guard let `self` = self else { return }
+            switch result {
+            case .error:
+                self.nsfwLabel.text = "Detection Failed"
+            case let .success(nsfwConfidence: confidence):
+                self.nsfwLabel.text = String(format: "%.1f %% porn", confidence * 100.0)
+            }
+        }
+        
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func closeButtonTask(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
-    */
-
 }
--- ml-cicd/Controllers/ViewController.swift
+++ ml-cicd/Controllers/ViewController.swift
@@ -10,11 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var photoButton: UIButton!
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    private var selectedImage: UIImage?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.photoButton.layer.cornerRadius = 12
+        self.cameraButton.layer.cornerRadius = 12
+        
+        photoButton.addTarget(self, action: #selector(showImagePickerTask), for: .touchUpInside)
     }
 
+    @objc func showImagePickerTask() {
+        let picker = UIImagePickerController()
+        
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        
+        self.present(picker, animated: true, completion: nil)
+    }
 
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[UIImagePickerController.InfoKey.editedImage] as? UIImage ?? info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {
+            return
+        }
+        
+        self.selectedImage = image
+        picker.dismiss(animated: true) {
+            self.performSegue(withIdentifier: "ImageViewer", sender: self)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "ImageViewer", let imageViewController = segue.destination.children.first as? ImageViewController else {
+            return
+        }
+        
+        imageViewController.image = self.selectedImage
+        self.selectedImage = nil
+    }
 }
 
